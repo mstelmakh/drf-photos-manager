d1e8f384690232e16a6d02c9bc15e264f74cd46b
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2022-10-25 17:03:38 +0200

    Add Photos app & models

--- src/app/photos/models.py
+++ src/app/photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from hexfield.fields import HexField
 
-# Create your models here.
+
+class Photo(models.Model):
+    title = models.CharField(max_length=255)
+    album_id = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    color = HexField(verbose_name="Dominant color")
+    URL = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.id}. {self.title}"
