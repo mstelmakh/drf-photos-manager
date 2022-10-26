9a152bc7b53f21826e16dbdf0787de2454a98296
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2022-10-26 15:01:01 +0200

    Add view to fetch photos from external API

--- src/app/photos/urls.py
+++ src/app/photos/urls.py
@@ -2,6 +2,8 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from . import views
 
+from .views import FetchPhotos
+
 
 router = DefaultRouter()
 
@@ -10,4 +12,6 @@ router.register('photos', views.PhotoView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('photos/fetch/all/', FetchPhotos.as_view()),
+    path('photos/fetch/<int:n>/', FetchPhotos.as_view()),
 ]
--- src/app/photos/views.py
+++ src/app/photos/views.py
@@ -1,9 +1,23 @@
 from rest_framework import viewsets
+from rest_framework import views
+from rest_framework.response import Response
 
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
+
+from fetching.services import fetch_photos_to_database
 
 
 class PhotoView(viewsets.ModelViewSet):
     serializer_class = PhotoSerializer
     queryset = Photo.objects.all()
+
+
+class FetchPhotos(views.APIView):
+
+    def get(self, request, n=None, *args, **kwargs):
+        if n:
+            photos = fetch_photos_to_database(n)
+        else:
+            photos = fetch_photos_to_database()
+        return Response(photos)
