f11844680d3d3a3922ed92d53843bc3e50a4f433
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2022-10-27 12:09:34 +0200

    Add docker

---
+++ src/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+if [ "$DEBUG" = "FALSE" ]
+then
+  echo "Running migrations"
+  python manage.py migrate
+fi
+
+exec "$@"
